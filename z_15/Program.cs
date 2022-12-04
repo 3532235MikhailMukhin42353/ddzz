7c9e19dca13e981d9116f8c411d42a371dc1fcf7
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2022-12-04 17:26:39 +0300

    add z_15 dfg :wq

---
+++ z_15/Program.cs
@@ -0,0 +1,13 @@
+﻿//Принимает цифру, обозначающую день недели и проверяет, является этот день выходным.
+try
+{
+    Console.WriteLine("Введите число дня недели");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x>0 && x<6) Console.WriteLine("Рабочий день недели");
+    else if (x>5 && x<8) Console.WriteLine("Выходной");
+    else Console.WriteLine("Некорректная дата");
+}
+catch
+{
+    Console.WriteLine("Введите именно число дня недели!");
+}

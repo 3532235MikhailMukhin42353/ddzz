30e227640d7ef8a0899ab0d56b001396809ec638
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2022-12-04 17:20:15 +0300

    add z_13 vjg :wq

---
+++ z_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+try
+{
+    Console.WriteLine("Введите трехзначное число");
+    int X = Convert.ToInt32(Console.ReadLine());
+    int numberA = X % 10;
+    if (X > 99 && X < 1000) Console.WriteLine($"Заданная цифра трехзначная. Третья её цифра = {numberA} ");
+    else Console.WriteLine("Заданная цифра не трехзначная!");
+}
+catch
+{
+    Console.WriteLine("Надо вводить именно целые числа!");
+}

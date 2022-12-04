3589d9d82a32daa07565982d39c735c03999d851
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2022-12-04 17:18:12 +0300

    add z_10 ssgsf :wq

---
+++ z_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа.
+try
+{
+    Console.WriteLine("Введите трехзначное число");
+    int X = Convert.ToInt32(Console.ReadLine());
+    int numberA = X / 10;    
+    int numberB = numberA % 10;
+    if (X > 99 && X < 1000) Console.WriteLine($"Заданная цифра трехзначная. Вторая её цифра = {numberB} ");
+    else Console.WriteLine("Заданная цифра не трехзначная!");
+}
+catch
+{
+    Console.WriteLine("Нужно вводить именно цифры!");
+}
